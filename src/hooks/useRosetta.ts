11d8c5105329695370d570ac0eac41f04157f0a9
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-17 23:35:45 +0100

    Update useRosetta.ts

--- src/hooks/useRosetta.ts
+++ src/hooks/useRosetta.ts
@@ -1,1 +1,10 @@
-placeholder
+import { useContext } from 'preact/hooks';
+import { RosettaContext, RosettaContextType } from '../RosettaProvider';
+
+export function useRosetta(): RosettaContextType {
+  const context = useContext(RosettaContext);
+  if (!context) {
+    throw new Error('useRosetta must be used within a RosettaProvider');
+  }
+  return context;
+}
