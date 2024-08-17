835452304362696144ddf8594b5753b614d6a6d5
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-17 23:39:54 +0100

    Update memoizedTranslations.ts

--- src/utils/memoizedTranslations.ts
+++ src/utils/memoizedTranslations.ts
@@ -1,1 +1,12 @@
-placeholder
+export function memoizedTranslations<T extends Record<string, any>>(translations: T): T {
+  const memoized: Partial<T> = {};
+  
+  Object.keys(translations).forEach(key => {
+    Object.defineProperty(memoized, key, {
+      get: () => translations[key],
+      enumerable: true,
+    });
+  });
+
+  return memoized as T;
+}
