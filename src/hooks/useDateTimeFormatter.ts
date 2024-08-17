617d1a08c70fd1f1f4d04a2d2e6d974769d8bf93
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-17 23:37:30 +0100

    Update useDateTimeFormatter.ts

--- src/hooks/useDateTimeFormatter.ts
+++ src/hooks/useDateTimeFormatter.ts
@@ -1,1 +1,10 @@
-placeholder
+import { useCallback } from 'preact/hooks';
+import { useRosetta } from './useRosetta';
+
+export function useDateTimeFormatter(options: Intl.DateTimeFormatOptions = {}) {
+  const { locale } = useRosetta();
+  
+  return useCallback((date: Date | number) => {
+    return new Intl.DateTimeFormat(locale, options).format(date);
+  }, [locale, options]);
+}
