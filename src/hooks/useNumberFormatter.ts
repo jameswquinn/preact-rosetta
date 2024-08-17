76c7ab311c13277380cd4d418dfcb9666cff905b
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-17 23:38:00 +0100

    Update useNumberFormatter.ts

--- src/hooks/useNumberFormatter.ts
+++ src/hooks/useNumberFormatter.ts
@@ -1,1 +1,10 @@
-placeholder
+import { useCallback } from 'preact/hooks';
+import { useRosetta } from './useRosetta';
+
+export function useNumberFormatter(options: Intl.NumberFormatOptions = {}) {
+  const { locale } = useRosetta();
+  
+  return useCallback((number: number) => {
+    return new Intl.NumberFormat(locale, options).format(number);
+  }, [locale, options]);
+}
