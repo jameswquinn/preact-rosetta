97f2834461389a9a76b15f329fbfec4b4aea5d0c
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-17 23:37:00 +0100

    Update useLocale.ts

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,1 +1,6 @@
-placeholder
+import { useRosetta } from './useRosetta';
+
+export function useLocale() {
+  const { locale, setLocale, loading } = useRosetta();
+  return { locale, setLocale, loading };
+}
